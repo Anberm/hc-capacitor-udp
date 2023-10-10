8573560e6ae365ac34ab86ec917c43fb3d5d20a1
Anber <shuangyan_m@hotmail.com>
2023-10-10 16:34:51 +0800

    feat: udp plugin

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,23 @@ const UdpPlugin = registerPlugin<UdpPluginPlugin>('UdpPlugin', {
   web: () => import('./web').then(m => new m.UdpPluginWeb()),
 });
 
+export const UdpPluginUtils = {
+  bufferToString: function (buffer: ArrayBuffer): string {
+      const charcodes = new Uint8Array(buffer);
+      //  btoa(String.fromCharCode.apply(null, charcodes));
+      return new TextDecoder("utf-8").decode(charcodes);
+  },
+  stringToBuffer: function (base64String: string): ArrayBuffer {
+      const str = atob(base64String);
+      const buf = new ArrayBuffer(str.length);
+      const bufView = new Uint8Array(buf);
+      for (let i = 0, strLen = str.length; i < strLen; i++) {
+          bufView[i] = str.charCodeAt(i);
+      }
+      return buf;
+  }
+}
+
+
 export * from './definitions';
 export { UdpPlugin };
